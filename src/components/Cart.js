1b127813df39c342e7224c2dcfd540bc6a1a2a80
Randark <koldaev.alexey@gmail.com>
2022-08-14 23:29:02 +0300

    Добавил состояния для пустой корзины и закладок

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -2,61 +2,80 @@ function Cart({ closeCart, removeItem, items = [], cartTotalPrice }) {
     return (
         <div className="cart_wrapper">
             <div className="cart">
-                <h2 className="d-flex justify-between">
-                    Корзина{" "}
-                    <img
-                        onClick={closeCart}
-                        className="btn_remove"
-                        src="img/btn_remove.svg"
-                        alt="remove"
-                    />
-                </h2>
-                <div className="cart_items_wrapper">
-                    {items.map((card) => (
-                        <div className="cart_item d-flex align-center">
+                {items.length === 0 ? (
+                    <div className="cart_empty">
+                        <img
+                            src="/img/empty_cart.jpg"
+                            alt="cart"
+                            width={120}
+                            height={120}
+                        />
+                        <h2>Корзина пустая</h2>
+                        <span>
+                            Добавьте хотя бы одну пару <br /> кроссовок, чтобы
+                            сделать заказ.
+                        </span>
+                        <button onClick={closeCart}>Вернуться назад</button>
+                    </div>
+                ) : (
+                    <>
+                        <h2 className="d-flex justify-between">
+                            Корзина{" "}
                             <img
-                                className="mr-20"
-                                width={70}
-                                height={70}
-                                src={card.imageUrl}
-                                alt="item"
-                            />
-                            <div>
-                                <p className="mb-5">{card.title}</p>
-                                <b>{card.price} руб.</b>
-                            </div>
-                            <img
-                                onClick={() => removeItem(card)}
+                                onClick={closeCart}
                                 className="btn_remove"
                                 src="img/btn_remove.svg"
                                 alt="remove"
                             />
+                        </h2>
+                        <div className="cart_items_wrapper">
+                            {items.map((card) => (
+                                <div className="cart_item d-flex align-center">
+                                    <img
+                                        className="mr-20"
+                                        width={70}
+                                        height={70}
+                                        src={card.imageUrl}
+                                        alt="item"
+                                    />
+                                    <div>
+                                        <p className="mb-5">{card.title}</p>
+                                        <b>{card.price} руб.</b>
+                                    </div>
+                                    <img
+                                        onClick={() => removeItem(card)}
+                                        className="btn_remove"
+                                        src="img/btn_remove.svg"
+                                        alt="remove"
+                                    />
+                                </div>
+                            ))}
                         </div>
-                    ))}
-                </div>
 
-                <div className="total_wrapper">
-                    <ul>
-                        <li className="d-flex justify-between">
-                            <span>Итого:</span>
-                            <div></div>
-                            <b>{cartTotalPrice(items)} руб.</b>
-                        </li>
-                        <li className="d-flex justify-between">
-                            <span>Налог 5%:</span>
-                            <div></div>
-                            <b> руб.</b>
-                        </li>
-                    </ul>
-                    <button>
-                        <img
-                            className="btn_arrow"
-                            src="img/arrow.svg"
-                            alt="arrow"
-                        />
-                        Оформить заказ
-                    </button>
-                </div>
+                        <div className="total_wrapper">
+                            <ul>
+                                <li className="d-flex justify-between">
+                                    <span>Итого:</span>
+                                    <div></div>
+                                    <b>{cartTotalPrice(items)} руб.</b>
+                                </li>
+                                <li className="d-flex justify-between">
+                                    <span>Налог 5%:</span>
+                                    <div></div>
+                                    <b> руб.</b>
+                                </li>
+                            </ul>
+                            <button>
+                                <img
+                                    className="btn_arrow"
+                                    src="img/arrow.svg"
+                                    alt="arrow"
+                                />
+                                Оформить заказ
+                            </button>
+                        </div>
+                    </>
+                )}
             </div>
         </div>
     );
--- src/pages/Favorites.jsx
+++ src/pages/Favorites.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import Card from "../components/Card";
 import StoreContext from "../context";
 
@@ -11,13 +12,26 @@ function Favorites({ handlerAddToFavorite }) {
                 <h1>Избранное</h1>
             </div>
             <div className="cards_container d-flex flex-wrap">
-                {favoriteItems.map((item) => (
-                    <Card
-                        favorited={true}
-                        addToFavorite={handlerAddToFavorite}
-                        {...item}
-                    />
-                ))}
+                {favoriteItems.length === 0 ? (
+                    <div className="favorite_empty">
+                        <img src="/img/empty_favorite.png" alt="smile" />
+                        <h2>Закладок нет :(</h2>
+                        <span>Вы ничего не добавляли в закладки</span>
+                        <Link to="/">
+                            <button>Вернуться назад</button>
+                        </Link>
+                    </div>
+                ) : (
+                    <>
+                        {favoriteItems.map((item) => (
+                            <Card
+                                favorited={true}
+                                addToFavorite={handlerAddToFavorite}
+                                {...item}
+                            />
+                        ))}
+                    </>
+                )}
             </div>
         </div>
     );
